c0f0c61e0641826e2c2aab9ff2b7ebd97305f8da
ThibaultDct <thibault.doucet44@gmail.com>
2020-11-16 12:03:18 +0100

    :sparkles: TP2 cours Java

---
+++ approche-imperative/src/fr/declaration/variable/DeclarationApp.java
@@ -0,0 +1,25 @@
+package fr.declaration.variable;
+
+public class DeclarationApp {
+
+    public static void main(String[] args){
+        byte byteVar = 2;
+        short shortVar = 3;
+        int intVar = 4;
+        long longVar = 5;
+        float floatVar = 6;
+        double doubleVar = 7;
+        boolean booleanVar = true;
+        String randomString = "test\ntest apres retour a la ligne";
+
+        System.out.println("Valeur byte : " + byteVar);
+        System.out.println("Valeur short : " + shortVar);
+        System.out.println("Valeur int : " + intVar);
+        System.out.println("Valeur long : " + longVar);
+        System.out.println("Valeur float : " + floatVar);
+        System.out.println("Valeur double : " + doubleVar);
+        System.out.println("Valeur boolean : " + booleanVar);
+        System.out.println("Valeur string : " + randomString);
+    }
+
+}
