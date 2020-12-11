ade62cd3d18dbe1350cddb8809d372496059be1e
Thibault Doucet <thibault.doucet44@gmail.com>
2020-12-11 09:52:03 +0100

    :sparkles: Add TP3 exercises

--- approche-objet/src/main/java/entites/Personne.java
+++ approche-objet/src/main/java/entites/Personne.java
@@ -6,10 +6,43 @@ public class Personne {
     String prenom;
     AdressePostale adresse;
 
-    public Personne(String nom, String prenom, AdressePostale adresse){
+    public Personne(String nom, String prenom){
         this.nom = nom;
         this.prenom = prenom;
+    }
+
+    public Personne(String nom, String prenom, AdressePostale adresse){
+        this(nom, prenom);
         this.adresse = adresse;
+    }
+
+    public void afficher(){
+        System.out.println("Nom : " + this.nom.toUpperCase());
+        System.out.println("Prénom : " + this.prenom);
+    }
+
+    public void setNom(String nom){
+        this.nom = nom;
+    }
+
+    public void setPrenom(String prenom){
+        this.prenom = prenom;
+    }
+
+    public void setAdresse(AdressePostale adresse){
+        this.adresse = adresse;
+    }
+
+    public String getNom(){
+        return this.nom;
+    }
+
+    public String getPrenom(){
+        return this.prenom;
+    }
+
+    public AdressePostale getAdresse(){
+        return this.adresse;
     }
 
 }
--- approche-objet/src/main/java/essais/TestPersonne.java
+++ approche-objet/src/main/java/essais/TestPersonne.java
@@ -6,8 +6,10 @@ import entites.Personne;
 public class TestPersonne {
 
     public static void main(String[] args) {
-        Personne personne1 = new Personne("DOUCET", "Thibault", new AdressePostale(52, "rue du Coudray", "44000", "Nantes"));
-        Personne personne2 = new Personne("DOUCET", "Nathan", new AdressePostale(33, "rue Principale", "72190", "Neuville sur Sarthe"));
+        Personne personne1 = new Personne("DOUCET", "Thibault");
+        Personne personne2 = new Personne("DOUCET", "Nathan");
+        personne1.setAdresse(new AdressePostale(52, "rue du Coudray", "44000", "Nantes"));
+        personne2.setAdresse(new AdressePostale(33, "rue Principale", "72190", "Neuville sur Sarthe"));
     }
 
 }
